6fe0fd55e83663ebc2b0c2178fab2752d3fdc8cd
Duc-Vi Nguyen <duc-vi97@hotmail.com>
2025-09-01 18:13:47 +0200

    noen kommentarer

--- script.js
+++ script.js
@@ -2,18 +2,23 @@ const input = document.getElementById("oppgave_inp");
 const button = document.getElementById("leggtil_btn");
 const todoliste = document.getElementById("todo_liste");
 
+//adder event for når man trykker på "legg til" knappen
 button.addEventListener("click", () =>{
+    //lagrer teksten som brukeren har skrevet.
     const text = input.value.trim();
+    //hvis teksten er tom send en melding tilbake.
     if (text === ""){
         alert("skriv noe");
         return;
     }
-
+    //ny ellement som er li type, 
     const li = document.createElement("li");
 
+    //lager en checkboks
     const checkBox = document.createElement("input");
     checkBox.type = "checkbox";
 
+    //
     const spanText = document.createElement("span");
     spanText.classList.add("text");
     spanText.textContent = text;
